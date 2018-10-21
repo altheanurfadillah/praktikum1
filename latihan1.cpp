86794bdb2450fd48f1916bfdc4529f4a1619894c
altheanurfadillah <al.shenanda@gmail.com>
2018-10-21 10:08:15 +0700

    upload

---
+++ latihan1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+ using namespace std;
+
+ int main() {
+     int p, l, luas;
+
+	cout<< "masukkan panjang: ";
+	cin >> p;
+	cout<< "masukkan lebar; ";
+	cin >> l;
+
+	luas = p*l;
+
+	cout <<"luas persegi panjang adalah: " << luas << endl ;
+	return 0;
+ }
